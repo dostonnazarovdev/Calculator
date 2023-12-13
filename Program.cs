b569865f3b3443cbec0d7029dcef84029b58cd3d
DostonNazarov <dostondev4477@gmail.com>
2023-12-13 16:02:16 +0500

    Add C# File

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+       static void Main()
+       {
+       Console.WriteLine("hello world");
+        System.Console.WriteLine("hello world2");
+        System.Console.Write("salom dasturchi");
+
+        
+        System.Console.Write(" salom");
+       }
+    }
+}
