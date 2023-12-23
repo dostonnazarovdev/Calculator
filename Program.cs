d9dc50be08ffb8a3e9316f958a596db93838f507
DostonNazarov <dostondev4477@gmail.com>
2023-12-23 20:49:36 +0500

    Afrimatic operators commit

--- Program.cs
+++ Program.cs
@@ -7,15 +7,23 @@ namespace Calculator
     {
        static void Main()
        {
-        Console.WriteLine("hello world1");
-        Console.WriteLine("hello world2");
-        Console.Write("hello world3\n");
+        Console.WriteLine("Enter values: ");
+        Console.Write("Enter firstNumber: ");
+        string firstNumber = Console.ReadLine();
 
+        Console.Write("Enter secondNumber: ");
+        string secondNumber = Console.ReadLine();
 
+        Console.WriteLine("Converting values in progress.....");
+        decimal firstUserValues = Convert.ToDecimal(firstNumber);
+        decimal secondUserValues=Convert.ToDecimal(secondNumber);
 
-
-        Console.WriteLine("Hello world4");
-        Console.ReadLine();
+        Console.WriteLine("Here are your results");
+        Console.WriteLine($"{firstUserValues} + {secondUserValues} = {firstUserValues + secondUserValues}");
+        Console.WriteLine($"{firstUserValues} - {secondUserValues} = {firstUserValues - secondUserValues}");
+        Console.WriteLine($"{firstUserValues} * {secondUserValues} = {firstUserValues * secondUserValues}");
+        Console.WriteLine($"{firstUserValues} / {secondUserValues} = {firstUserValues / secondUserValues}");
+        Console.WriteLine($"{firstUserValues} % {secondUserValues} = {firstUserValues % secondUserValues}");
        }
     }
 }
--- obj/Debug/net8.0/CALCULATOR.AssemblyInfo.cs
+++ obj/Debug/net8.0/CALCULATOR.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CALCULATOR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b569865f3b3443cbec0d7029dcef84029b58cd3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0526b2a2702aeaa137e7c30bbb89ff2554e29b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CALCULATOR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CALCULATOR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
